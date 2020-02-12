d62588341edfa07dfec69bba6441f1e873955926
Hal Jundzil <ha510666@ucf.edu>
2020-02-12 12:01:14 -0500

    #95

---
+++ demo/es6.js
@@ -0,0 +1,15 @@
+class Cat {
+    speak = () => {
+      return ''
+    }
+
+    render () {
+      return 'String'
+    }
+
+    newMethod () {
+      return 0
+    }
+}
+const Luna = new Cat()
+Luna.speak()
