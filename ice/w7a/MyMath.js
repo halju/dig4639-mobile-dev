553078d197c6e1ec252a894f2ff586bc9c78c4f8
Hal Jundzil <ha510666@ucf.edu>
2020-02-17 12:23:31 -0500

    Jest

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,7 +1,63 @@
 function Sum (a, b) {
-  return a + b
+  let result = undefined
+  if(typeof a == "number" && typeof b == "number") {
+    result = a + b
+  }
+  return result;
+}
+//console.log(Sum(2,3)==5);
+//console.log(Sum(-10,-20)==-30);
+//console.log(isNaN(Sum(-10,"Twenty")));
+
+function AddList (array) {
+  let result = undefined;
+  if(Array.isArray(array) && array.length > 0) {
+    result = 0;
+    for(var i = 0; i < array.length; i++) {
+      if(typeof array[i] != "number") {
+        result = undefined;
+        break;
+      }
+      result = result + array[i];
+    }
+  }
+  return result;
+} 
+function DivideBy (a, b) {
+  let result = undefined;
+  if(typeof a == "number" && typeof b == "number" && b != 0) {
+    result = a/b;
+  }
+  return result;
+}
+function ContainsString (a, b) {
+  let result = undefined;
+  if(typeof a == "string" && typeof b == "string" && a.indexOf(b) !== -1) {
+    result = true;
+  }
+  return result;
 }
 
-console.log(Sum(2,3));
+//function ReSortedNumbers () {}
+
+function Adder (a, b) {
+  class Adder {
+    constructor (props) {
+        this.props = props;
+    }
+    sum() {
+        return this.props.a + this.props.b;
+    }
+    render() {
+        return (
+            `<p>The sum of ${this.props.a} and ${this.props.b} is ${this.sum()}.</p>`
+        );
+    }
+  }
+}
 
 export { Sum };
+export { AddList };
+export { DivideBy };
+export { ContainsString };
+export { Adder };
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,10 +1,68 @@
 import { Sum } from './MyMath.js'
+import { AddList } from './MyMath.js'
+import { DivideBy } from './MyMath.js'
+import { ContainsString } from './MyMath.js'
+import { Adder } from './MyMath.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+describe("Sum", () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3)
+  })
+  
+  it('produces the sum of 10 + 20, which should be 30', () => {
+    expect(Sum(10, 20)).toBe(30)
+  })
 })
 
 // ICE 
+
+describe("AddList", () => {
+  test('Adds all numbers in list', () => {
+    let numElem = 1000;
+    let input = [];
+    let result = 0;
+    for (var i = 0; i < numElem; i++) {
+      var elem = Math.random()*1000;
+      result += elem;
+      input.push(elem);
+    }
+    expect(AddList(input)).toBe(result);
+    let input2 = [15,5,8,-10];
+    let result2 = 18;
+    expect(AddList(input2)).toBe(result2);
+  })
+  test('Test if empty array returns undefined', () => {
+    let input = [];
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('Test if undefined element returns undefined', () => {
+    let input = [undefined,0,2,3];
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('Test if non array returns undefined', () => {
+    let input = 100;
+    expect(AddList(input)).toBeUndefined();
+  })
+})
+
+
+describe("DivideBy", () => {
+  test('Divides two numbers', () => {
+    expect(DivideBy(6, 3)).toBe(2)
+  })
+})
+
+describe("ContainsString", () => {
+  test('Returns true if the first parameter has as a substring the second parameter', () => {
+    expect(ContainsString("snowbird", "snow")).toBe(true)
+  })
+})
+
+/*  describe("Adder", () => {
+  test('adds 5 + 6 to equal 11', () => {
+    expect(Adder(this.props.5, this.props.6)).toBe(this.sum())
+  })
+}) */
 
 // Examples for Null
 test('null', () => {
