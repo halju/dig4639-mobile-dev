72a67559040ed8be5ff660bd84c6754ee4a909f9
halju <hjundzil@knights.ucf.edu>
2020-03-26 21:16:52 -0400

    lab5

--- labs/5/src/App.js
+++ labs/5/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {content:"", list: []}
+  }
+  
+  async getServerData() {
+    // const response = await fetch("http://localhost:3001")
+    const response = await fetch("https://api.weather.gov/gridpoints/MLB/25,69/forecast")
+    console.log("Received response from server!")
+    console.log(response)
+    let obj = await response.json()
+    console.log("Processed response as JSON: ", obj)
+    // this.setState({content:obj.b})
+    this.setState({list:obj})
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    console.log("Component did mount!")
+    this.getServerData()
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.content}</p>
+        {this.state.list.map((listObject, index) =>
+        <Card key={index} title={listObject.setup}>
+          {listObject.punchline}
+          </Card>
+        )}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ labs/5/src/components/Card/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Card extends React.Component {
+  render() {
+    return (
+    <div className='card'>
+      <h2>{this.props.title}</h2>
+      {this.props.children}
+    </div>)
+  }
+}
+
+export default Card
