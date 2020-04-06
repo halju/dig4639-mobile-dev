7efa2d92f8d168699f0af75242e1efc9e5ed8e4d
halju <hjundzil@knights.ucf.edu>
2020-04-06 15:05:48 -0400

    lab6

--- labs/6/components/Home/index.js
+++ labs/6/components/Home/index.js
@@ -4,81 +4,56 @@ import questions from './questions.json';
 
 const TITLE_STATE = 0
 const QUESTION_STATE = 1
-const TIME_LIMIT = 5
+const TIME_LIMIT = 10
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          score: 0,
-          titleText: "Welcome to the Rat Quiz!",
-          counter: 0,
-          currentState: TITLE_STATE,
-          currentQuestion: 0
-        }
-        this.timeLimit = TIME_LIMIT
+  constructor(props) {
+    super(props)
+    this.state = {titleText: "Welcome to the Rodent Quiz!",
+    currentState: TITLE_STATE,
+    counter: 0,
+    currentQuestion: 0}
+    this.counter = 0
+    this.timeLimit = TIME_LIMIT
+  }
+  nextQuestion() {
+    clearInterval(this.timer)
+    this.setState({
+      titleText:"You answered early!",
+      currentState: TITLE_STATE
+    })
+  }
+
+  start() {
+    console.log("Starting!")
+    this.setState({counter:0})
+    this.setState({currentState: QUESTION_STATE})
+    this.timer = setInterval(() => { 
+      console.log("INTERVAL CALLED")
+      this.setState({counter : this.state.counter+1})
+      if(this.state.counter < this.timeLimit) {
+        this.setState({titleText:"Begin the quiz! " + this.state.counter})
+      } else {
+        this.setState({titleText:"Time's up!"})
+        clearInterval(this.timer)
       }
-      nextQuestion(correct) {
-        console.log("BUTTON PRESSED")
-        if(correct) {
-          this.setState({score: this.state.score+1})
-        }
-        if(this.state.currentQuestion === questions.length - 1) {
-          console.log("DONE")
-        } else {
-          clearInterval(this.timer)
-          console.log(this.state.currentQuestion)
-          this.setState({
-            titleText:"You answered x",
-            currentState: QUESTION_STATE,
-            currentQuestion: this.state.currentQuestion + 1
-          })
-        }
-      }
-      countdown() {
-          console.log("INTERVAL CALLED")
-          console.log(this.state.counter)
-          if(this.state.counter < this.timeLimit) {
-            this.setState({titleText:'Starting the quiz',
-            counter: this.state.counter + 1
-            })
-          } else {
-            this.setState({titleText:"Beginning quiz!",
-            currentState: QUESTION_STATE,
-            counter: 0
-            })
-            if(this.state.currentState === TITLE_STATE) {
-              this.timer = setInterval(() => this.countdown(), 1000)
-              clearInterval(this.timer)
-            } else {
-              this.setState({titleText:"You answered!"})
-            }
-        }
-      }
-      start() {
-        console.log("Starting!")
-        this.setState({titleText: "Starting the quiz!", counter:0})
-        this.timer = setInterval(() => this.countdown(), 1000)
-      }
-      render() {
-        return (
-          <>
-          <div className='App'>
-            <p>{this.timeLimit - this.state.counter}</p>
-            {((this.state.currentState === TITLE_STATE) ?
-            <>
-            <h2>{this.state.titleText}</h2>
-            <input id="startButton" type="button" value="start" onClick={()=>this.start()}
-            />
-            </>
-            :
-            <QuizQuestion answers={questions[0].possibleAnswers} question=
-            {questions[0].question} nextQuestion={(correct) => this.nextQuestion()}
-            ></QuizQuestion>)}
-            <p>Score: {this.state.score}</p>
-          </div>
-          </>)
-      }
-    }
+    }, 1000);
+  }
+  render(props) {
+    console.log("RENDER CALLED")
+    return (
+      <div className='App'>
+        <div>{this.timeLimit - this.state.counter}</div>
+      {(this.state.currentState === QUESTION_STATE) ?
+        <Questions answers={questions[this.state.currentQuestion].possibleAnswers} 
+        question={questions[this.state.currentQuestion].question} 
+        nextQuestion={() => this.nextQuestion()} />
+        : 
+        <h1 className='title'>{this.state.titleText}</h1>}
+        <input id='startButton' type="button" value="Start" onClick={() => this.start()}></input>
+      </div>
+    )
+  }
+}
 
 export default Home;
--- labs/6/components/Questions/index.js
+++ labs/6/components/Questions/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import questions from './questions.json';
+import React from 'react'
 
 class Questions extends React.Component {
-  render() {
-    return(
-      <>
-      <h2>{this.props.question}</h2>
-      {this.props.answers.map((v) => {
-        return <input type="button"
-        value={v.text}
-        className="answerButton"
-        onClick={() => this.props.nextQuestion()}></input>
-      })}
-      </>
+render() {
+  return( 
+    <>
+    <h2>{this.props.question}</h2>
+    {this.props.answers.map((v) => {
+      return <input type="button" 
+      value={v.text} 
+      className="answerButton" 
+      onClick={() => this.props.nextQuestion()}></input>
+    })}
+    </>
     )
   }
 }
 
-export default Questions;
+export default Questions
